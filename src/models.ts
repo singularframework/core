ff6430649020a32ec7d7d677bb42cb8e228d27cd
Ramtin Soltani <ramtinsoltani93@gmail.com>
2020-08-26 09:47:38 -0700

    Added validation definition types and executable validators type.

--- src/models.ts
+++ src/models.ts
@@ -1,2 +1,20 @@
 export type ValidatorFunction = (value: any, rawValues?: any) => boolean|Error;
 export type AsyncValidatorFunction = (value: any, rawValues?: any) => Promise<boolean|Error>;
+
+export interface ExecutableValidators {
+
+  __exec(): AsyncValidatorFunction;
+
+}
+
+export interface BodyValidationDefinition {
+
+  [key: string]: AsyncValidatorFunction|ValidatorFunction|BodyValidationDefinition;
+
+}
+
+export interface ValidationDefinition {
+
+  [key: string]: AsyncValidatorFunction|ValidatorFunction;
+
+}
