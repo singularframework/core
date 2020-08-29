16f8512d62636057a357fa5799162ea5fdca945a
Ramtin Soltani <ramtinsoltani93@gmail.com>
2020-08-29 04:25:55 -0700

    Removed pipe function factory type; added async pipe function type; added executable pipes interface.

--- src/models.ts
+++ src/models.ts
@@ -1,11 +1,17 @@
 export type PipeFunction = (value: any, rawValues?: any) => Omit<void, any>;
-export type PipeFunctionFactory = (...args: any[]) => PipeFunction;
+export type AsyncPipeFunction = (value: any, rawValues?: any) => Promise<Omit<void, any>>;
 export type ValidatorFunction = (value: any, rawValues?: any) => boolean|Error;
 export type AsyncValidatorFunction = (value: any, rawValues?: any) => Promise<boolean|Error>;
 
 export interface ExecutableValidators {
 
   __exec(): AsyncValidatorFunction;
+
+}
+
+export interface ExecutablePipes {
+
+  __exec(): AsyncPipeFunction;
 
 }
 
